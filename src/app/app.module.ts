3dfb67f51bb98f889762f25fe48c4bbe84a42be7
Joao Paulo <joaopaulogse@gmail.com>
2017-10-03 19:58:24 -0300

    configuracao do firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {AngularFireModule} from 'angularfire2'
+import configFirebase from '../config/configFirebase' 
 
 @NgModule({
   declarations: [
@@ -18,6 +20,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(configFirebase)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/config/configFirebase.ts
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDS3cWicyMq15nr8Ls-VuE8S_TtDFjrs8E",
+    authDomain: "biblioteca-3fbd6.firebaseapp.com",
+    databaseURL: "https://biblioteca-3fbd6.firebaseio.com",
+    projectId: "biblioteca-3fbd6",
+    storageBucket: "biblioteca-3fbd6.appspot.com",
+    messagingSenderId: "41221581830"
+}
