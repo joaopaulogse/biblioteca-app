9af4dcf979a2926a78065b22c3a154c1aa79860d
João Paulo <joaopaulogse@gmail.com>
2017-10-12 18:37:07 -0300

    correcão do click no botao

--- src/pages/book-register/book-register.ts
+++ src/pages/book-register/book-register.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
-
+import * as firebase from 'firebase';
 /**
  * Generated class for the BookRegisterPage page.
  *
