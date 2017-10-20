b163ad9f39618258ae5388299d8b6be7f9bcc708
Matheus <rodmatheus96@gmail.com>
2017-10-20 18:34:08 -0200

    Ajuste no botão de perfil.

--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -1,5 +1,5 @@
 import { Component} from '@angular/core'
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { UsuarioModel } from '../../models/UsuarioModel';
 import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase/app'
